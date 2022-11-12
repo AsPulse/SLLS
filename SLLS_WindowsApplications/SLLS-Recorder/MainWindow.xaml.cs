ff178477d4533a413480efd86ed807d129bab43d
AsPulse <aspulse.x@gmail.com>
2022-11-13 01:00:32 +0900

    Add: PlaybackInfo

--- SLLS_WindowsApplications/SLLS-Recorder/MainWindow.xaml.cs
+++ SLLS_WindowsApplications/SLLS-Recorder/MainWindow.xaml.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Media.Imaging;
 
@@ -9,16 +11,28 @@ namespace SLLS_Recorder {
     public partial class MainWindow : Window
     {
         readonly Camera camera;
+
+        private int frames = 0;
+        private Stopwatch time;
+
         public MainWindow()
         {
             InitializeComponent();
 
             camera = new Camera();
             camera.CameraImageRefreshed += Camera_ImageRefreshed;
+
+            time = new Stopwatch();
+            time.Start();
         }
 
         private void Camera_ImageRefreshed(object sender, WriteableBitmap bitmap) {
             CameraViewer.Source = bitmap;
+            PlaybackInfo.Content = string.Format("PlaybackInfo: fps={0}", Math.Round(++frames / time.Elapsed.TotalSeconds, 1));
+            if (time.ElapsedMilliseconds > 1000) {
+                frames = 0;
+                time.Restart();
+            }
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
