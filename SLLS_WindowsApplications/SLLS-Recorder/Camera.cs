6d7783a35a9d4d0e8e176447a5a54028363a1d1b
AsPulse <aspulse.x@gmail.com>
2022-11-14 01:31:35 +0900

    Add: RecordingButton

--- SLLS_WindowsApplications/SLLS-Recorder/Camera.cs
+++ SLLS_WindowsApplications/SLLS-Recorder/Camera.cs
@@ -13,16 +13,28 @@ using OpenCvSharp.WpfExtensions;
 using Size = OpenCvSharp.Size;
 
 namespace SLLS_Recorder {
+    enum RecordingStatus {
+        READY,
+        PREPARING_TO_START,
+        RECORDING,
+        PREPARING_TO_FINISH
+    };
+
     internal class Camera : IDisposable {
 
         public delegate void CameraImageRefreshedEventHandler(object sender, WriteableBitmap bitmap);
         public event CameraImageRefreshedEventHandler? CameraImageRefreshed;
+
+        public delegate void StatusChangedEventHandler(object sender, RecordingStatus status);
+        public event StatusChangedEventHandler? StatusChanged;
 
         private bool live = true;
 
         private readonly int width = 1920;
         private readonly int height = 1080;
         private readonly int fps = 24;
+
+        public RecordingStatus status = RecordingStatus.READY;
 
         private bool recording = false;
         private readonly int chunkLength = 240;
@@ -40,10 +52,6 @@ namespace SLLS_Recorder {
             new FFMpegWriter(fps, width, height, string.Format("./data/{0}.mp4", chunkId))
         );
             Task.Run(Worker_DoWork);
-            Task.Run(async () => {
-                await Task.Delay(3000);
-                //StartRecord();
-            });
         }
 
         public void SelectCamera(int id) {
@@ -104,8 +112,18 @@ namespace SLLS_Recorder {
             live = false;
             StopRecord().Wait();
         }
+
+        private void SetStatus(RecordingStatus newStatus) {
+            status = newStatus;
+            Application.Current.Dispatcher.Invoke(() => {
+                StatusChanged?.Invoke(this, newStatus);
+            });
+        }
+
         
         public void StartRecord() {
+            if (status != RecordingStatus.READY) return;
+            SetStatus(RecordingStatus.PREPARING_TO_START);
             chunkId = 0;
             renderedFrame = 0;
             dropFrames = 0;
@@ -114,10 +132,13 @@ namespace SLLS_Recorder {
                 sw.Restart();
                 recording = true;
                 Debug.WriteLine("Recording Start");
+                SetStatus(RecordingStatus.RECORDING);
             });
         }
 
         public Task StopRecord() {
+            if (status != RecordingStatus.RECORDING) return Task.CompletedTask;
+            SetStatus(RecordingStatus.PREPARING_TO_FINISH);
             recording = false;
             renderedFrame = 0;
             int finishChunk = chunkId;
@@ -125,6 +146,7 @@ namespace SLLS_Recorder {
             return Task.Run(async () => {
                 await vw.RenderChunk(finishChunk);
                 await vw.FreeAllChunk();
+                SetStatus(RecordingStatus.READY);
             });
         }
     }
--- SLLS_WindowsApplications/SLLS-Recorder/MainWindow.xaml.cs
+++ SLLS_WindowsApplications/SLLS-Recorder/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 using System.Windows.Media.Imaging;
+using static OpenCvSharp.Stitcher;
 
 
 namespace SLLS_Recorder {
@@ -20,6 +21,7 @@ namespace SLLS_Recorder {
 
             camera = new Camera();
             camera.CameraImageRefreshed += Camera_ImageRefreshed;
+            camera.StatusChanged += Camera_StatusChanged;
 
             List<string> cameras = new List<DsDevice>(DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice))
                 .Select(v => v.Name)
@@ -28,6 +30,24 @@ namespace SLLS_Recorder {
             int obsCameraIndex = cameras.FindIndex(v => v.Contains("OBS-Camera")); ;
 
             CameraSelection.SelectedIndex = obsCameraIndex >= 0 ? obsCameraIndex : 0;
+        }
+
+        private void Camera_StatusChanged(object sender, RecordingStatus status) {
+            RecordingButton.IsEnabled = status == RecordingStatus.READY || status == RecordingStatus.RECORDING;
+            switch (status) {
+                case RecordingStatus.READY:
+                    RecordingButton.Content = "録画";
+                    break;
+                case RecordingStatus.RECORDING:
+                    RecordingButton.Content = "停止 (録画中...)";
+                    break;
+                case RecordingStatus.PREPARING_TO_START:
+                    RecordingButton.Content = "録画開始処理中";
+                    break;
+                case RecordingStatus.PREPARING_TO_FINISH:
+                    RecordingButton.Content = "録画停止処理中";
+                    break;
+            }
         }
 
         private void Camera_ImageRefreshed(object sender, WriteableBitmap bitmap) {
@@ -40,6 +60,17 @@ namespace SLLS_Recorder {
 
         private void CameraSelection_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e) {
             camera.SelectCamera(CameraSelection.SelectedIndex);
+        }
+
+        private void RecodingButton_Click(object sender, RoutedEventArgs e) {
+            switch (camera.status) {
+                case RecordingStatus.READY:
+                    camera.StartRecord();
+                    break;
+                case RecordingStatus.RECORDING:
+                    camera.StopRecord();
+                    break;
+            }
         }
     }
 }
