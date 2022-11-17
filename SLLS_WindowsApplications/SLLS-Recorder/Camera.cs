e83a3f50a966e3c1abd14f7ea04a3b24f3f5a41e
AsPulse <84216737+AsPulse@users.noreply.github.com>
2022-11-18 07:01:15 +0900

    Feature/opencvsharp recorder (#2)
    * Add: Player
    
    * Add: disposable
    
    * Add: PlaybackInfo
    
    * Add: stopwatch-based frames
    
    * Add: dispose to stop record
    
    * Add: count drop frames
    
    * Add: chunkLength
    
    * Add: full properties
    
    * Add: Properties
    
    * Add: Camera list
    
    * Add: RecordingButton
    
    * Add: enforce using local ffmpeg
    
    * Add: WriteableBitmap Static
    
    * Add: Frame Statistics

---
+++ SLLS_WindowsApplications/SLLS-Recorder/Camera.cs
@@ -0,0 +1,154 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+using Size = OpenCvSharp.Size;
+
+namespace SLLS_Recorder {
+    enum RecordingStatus {
+        READY,
+        PREPARING_TO_START,
+        RECORDING,
+        PREPARING_TO_FINISH
+    };
+
+    internal class Camera : IDisposable {
+
+        public delegate void CameraImageRefreshedEventHandler(object sender);
+        public event CameraImageRefreshedEventHandler? CameraImageRefreshed;
+
+        public delegate void StatusChangedEventHandler(object sender, RecordingStatus status);
+        public event StatusChangedEventHandler? StatusChanged;
+
+        private bool live = true;
+
+        private readonly int width = 1920;
+        private readonly int height = 1080;
+        private readonly int fps = 24;
+
+        public RecordingStatus status = RecordingStatus.READY;
+
+        private bool recording = false;
+        public readonly int chunkLength = 240;
+        public int renderedFrame = 0;
+        public int chunkId = -1;
+
+        public int dropFrames = 0;
+
+        private FFMpegWriterController vw;
+        private VideoCapture? vc;
+        readonly Stopwatch sw = new();
+        public WriteableBitmap bmp;
+
+        public Camera() {
+            vw = new(chunkId =>
+                new FFMpegWriter(fps, width, height, string.Format("./data/{0}.mp4", chunkId))
+            );
+            bmp = new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgr24, null);
+            Task.Run(Worker_DoWork);
+        }
+
+        public void SelectCamera(int id) {
+            vc = null;
+            vc = new(id) {
+                FrameWidth = width,
+                FrameHeight = height
+            };
+
+            if (!vc.IsOpened()) {
+                MessageBox.Show("Can't use camera.");
+                return;
+            }
+        }
+
+        private void Worker_DoWork() {
+
+            Mat frame = new(width, height, MatType.CV_8UC3);
+
+            while (live) {
+                if (vc == null) continue;
+                vc.Read(frame);
+                if (frame.Empty()) continue;
+                Application.Current.Dispatcher.Invoke(() => {
+                    WriteableBitmapConverter.ToWriteableBitmap(frame, bmp);
+                    CameraImageRefreshed?.Invoke(this);
+                });
+
+                /**
+                 * Recording
+                 */
+                if (recording) {
+                    int nowFrame = (int) Math.Round(sw.Elapsed.TotalSeconds * fps);
+                    int skipped = nowFrame - renderedFrame - 1;
+                    if (skipped > 0) {
+                        dropFrames += skipped;
+                        Debug.WriteLine(string.Format("Dropped: {0} ( of {1} )",skipped, dropFrames));
+                    }
+                    while(renderedFrame < nowFrame && renderedFrame < chunkLength) {
+                        vw.GetVw(chunkId).Result.SetFrame(renderedFrame, frame);
+                        renderedFrame++;
+                    }
+                    if (renderedFrame >= chunkLength) {
+                        vw.RenderChunk(chunkId++);
+                        Task.Run(() => {
+                            Enumerable.Range(chunkId, 5).ToList().ForEach(x => vw.Ready(x));
+                        });
+                        renderedFrame = 0;
+                        sw.Restart();
+                    }
+                }
+            }
+        }
+
+        public void Dispose() {
+            if (!live) return;
+            live = false;
+            StopRecord().Wait();
+        }
+
+        private void SetStatus(RecordingStatus newStatus) {
+            status = newStatus;
+            Application.Current.Dispatcher.Invoke(() => {
+                StatusChanged?.Invoke(this, newStatus);
+            });
+        }
+
+        
+        public void StartRecord() {
+            if (status != RecordingStatus.READY) return;
+            SetStatus(RecordingStatus.PREPARING_TO_START);
+            chunkId++;
+            renderedFrame = 0;
+            dropFrames = 0;
+            Task.Run(async () => {
+                await Task.WhenAll(Enumerable.Range(chunkId, 5).Select(x => vw.GetVw(x)));
+                sw.Restart();
+                recording = true;
+                Debug.WriteLine("Recording Start");
+                SetStatus(RecordingStatus.RECORDING);
+            });
+        }
+
+        public Task StopRecord() {
+            if (status != RecordingStatus.RECORDING) return Task.CompletedTask;
+            SetStatus(RecordingStatus.PREPARING_TO_FINISH);
+            recording = false;
+            renderedFrame = 0;
+            int finishChunk = chunkId;
+            return Task.Run(async () => {
+                await vw.RenderChunk(finishChunk);
+                await vw.FreeAllChunk();
+                SetStatus(RecordingStatus.READY);
+            });
+        }
+    }
+}
--- SLLS_WindowsApplications/SLLS-Recorder/MainWindow.xaml.cs
+++ SLLS_WindowsApplications/SLLS-Recorder/MainWindow.xaml.cs
@@ -1,28 +1,79 @@
-﻿using System;
+using DirectShowLib;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using static OpenCvSharp.Stitcher;
 
-namespace SLLS_Recorder
-{
+
+namespace SLLS_Recorder {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly Camera camera;
         public MainWindow()
         {
             InitializeComponent();
+
+            camera = new Camera();
+            camera.CameraImageRefreshed += Camera_ImageRefreshed;
+            camera.StatusChanged += Camera_StatusChanged;
+            CameraViewer.Source = camera.bmp;
+
+            List<string> cameras = new List<DsDevice>(DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice))
+                .Select(v => v.Name)
+                .ToList();
+            cameras.ForEach(v => CameraSelection.Items.Add(v));
+            int obsCameraIndex = cameras.FindIndex(v => v.Contains("OBS-Camera")); ;
+
+            CameraSelection.SelectedIndex = obsCameraIndex >= 0 ? obsCameraIndex : 0;
+        }
+
+        private void Camera_StatusChanged(object sender, RecordingStatus status) {
+            RecordingButton.IsEnabled = status == RecordingStatus.READY || status == RecordingStatus.RECORDING;
+            switch (status) {
+                case RecordingStatus.READY:
+                    RecordingButton.Content = "録画";
+                    break;
+                case RecordingStatus.RECORDING:
+                    RecordingButton.Content = "停止 (録画中...)";
+                    break;
+                case RecordingStatus.PREPARING_TO_START:
+                    RecordingButton.Content = "録画開始処理中";
+                    break;
+                case RecordingStatus.PREPARING_TO_FINISH:
+                    RecordingButton.Content = "録画停止処理中";
+                    break;
+            }
+        }
+
+        private void Camera_ImageRefreshed(object sender) {
+            FrameCounter.Content = string.Format("Frames: {0} / {1} frames", camera.renderedFrame, camera.chunkLength);
+            DroppedCounter.Content = string.Format("Dropped: {0} frames", camera.dropFrames);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            camera.Dispose();
+        }
+
+        private void CameraSelection_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e) {
+            camera.SelectCamera(CameraSelection.SelectedIndex);
+        }
+
+        private void RecodingButton_Click(object sender, RoutedEventArgs e) {
+            switch (camera.status) {
+                case RecordingStatus.READY:
+                    camera.StartRecord();
+                    break;
+                case RecordingStatus.RECORDING:
+                    camera.StopRecord();
+                    break;
+            }
         }
     }
 }
