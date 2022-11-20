0b405e99d5a7c015e01884032dd541a6a5576eaa
AsPulse <aspulse.x@gmail.com>
2022-11-20 14:01:34 +0900

    Fix: move folder

--- SLLS_WindowsApplications/SLLS-Common/ManagedPayloads/DeviceId/AssignDeviceId.cs
+++ SLLS_WindowsApplications/SLLS-Common/ManagedPayloads/DeviceId/AssignDeviceId.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SLLS_Common.ManagedPayloads {
-    public class AssignDeviceId : ManagedPayload {
+namespace SLLS_Common.ManagedPayloads.DeviceId
+{
+    public class AssignDeviceId : ManagedPayload
+    {
         public const byte ENDPOINT = 0x01;
 
         public AssignDeviceId(TCPPayload? raw = null) : base(raw) { }
@@ -13,15 +15,18 @@ namespace SLLS_Common.ManagedPayloads {
 
         public byte TargetDeviceId { get; set; }
 
-        public static AssignDeviceId? Parse(TCPPayload payload) {
+        public static AssignDeviceId? Parse(TCPPayload payload)
+        {
             if (payload.Data[0] != ENDPOINT) return null;
-            return new (payload) {
+            return new(payload)
+            {
                 DeviceId = payload.Data[1],
                 TargetDeviceId = payload.Data[2],
             };
         }
 
-        public override SendablePayload SendData(byte ToDeviceId) {
+        public override SendablePayload SendData(byte ToDeviceId)
+        {
             return new(
                 new byte[] { ENDPOINT, DeviceId, ToDeviceId, TargetDeviceId },
                 ToDeviceId,
@@ -29,7 +34,8 @@ namespace SLLS_Common.ManagedPayloads {
             );
         }
 
-        public override string ToLogStringReceive() {
+        public override string ToLogStringReceive()
+        {
             return LogStringReceive("ASSIGN_DEVICEID");
         }
     }
--- SLLS_WindowsApplications/SLLS-Common/ManagedPayloads/DeviceId/RequestDeviceId.cs
+++ SLLS_WindowsApplications/SLLS-Common/ManagedPayloads/DeviceId/RequestDeviceId.cs
@@ -4,20 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SLLS_Common.ManagedPayloads {
-    public class RequestDeviceId : ManagedPayload {
+namespace SLLS_Common.ManagedPayloads.DeviceId
+{
+    public class RequestDeviceId : ManagedPayload
+    {
         public const byte ENDPOINT = 0x00;
 
         public RequestDeviceId(TCPPayload? raw = null) : base(raw) { }
 
-        public static RequestDeviceId? Parse(TCPPayload payload) {
+        public static RequestDeviceId? Parse(TCPPayload payload)
+        {
             if (payload.Data[0] != ENDPOINT) return null;
-            return new RequestDeviceId(payload) {
+            return new RequestDeviceId(payload)
+            {
                 DeviceId = payload.Data[1],
             };
         }
 
-        public override SendablePayload SendData(byte ToDeviceId) {
+        public override SendablePayload SendData(byte ToDeviceId)
+        {
             return new(
                 new byte[] { ENDPOINT, 0xFF, ToDeviceId },
                 ToDeviceId,
@@ -25,7 +30,8 @@ namespace SLLS_Common.ManagedPayloads {
             );
         }
 
-        public override string ToLogStringReceive() {
+        public override string ToLogStringReceive()
+        {
             return $"--> 0x{DeviceId:X2} REQUEST_DEVICEID";
         }
     }
